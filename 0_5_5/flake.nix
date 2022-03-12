3f5fc5b5d7c802ef2415a05de168c43e2e73891c
hugosenari <hugosenari@gmail.com>
2022-03-12 01:56:05 -0300

    chore: re index jupyternim

---
+++ 0_5_5/flake.nix
@@ -0,0 +1,58 @@
+{
+  description = ''A Jupyter kernel for nim.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-jupyternim-0_5_5.flake = false;
+  inputs.src-jupyternim-0_5_5.owner = "stisa";
+  inputs.src-jupyternim-0_5_5.ref   = "refs/tags/0.5.5";
+  inputs.src-jupyternim-0_5_5.repo  = "jupyternim";
+  inputs.src-jupyternim-0_5_5.type  = "github";
+  
+  inputs."zmq".dir   = "nimpkgs/z/zmq";
+  inputs."zmq".owner = "riinr";
+  inputs."zmq".ref   = "flake-pinning";
+  inputs."zmq".repo  = "flake-nimble";
+  inputs."zmq".type  = "github";
+  inputs."zmq".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."zmq".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."hmac".dir   = "nimpkgs/h/hmac";
+  inputs."hmac".owner = "riinr";
+  inputs."hmac".ref   = "flake-pinning";
+  inputs."hmac".repo  = "flake-nimble";
+  inputs."hmac".type  = "github";
+  inputs."hmac".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."hmac".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."nimsha2".dir   = "nimpkgs/n/nimsha2";
+  inputs."nimsha2".owner = "riinr";
+  inputs."nimsha2".ref   = "flake-pinning";
+  inputs."nimsha2".repo  = "flake-nimble";
+  inputs."nimsha2".type  = "github";
+  inputs."nimsha2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimsha2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."nimpng".dir   = "nimpkgs/n/nimpng";
+  inputs."nimpng".owner = "riinr";
+  inputs."nimpng".ref   = "flake-pinning";
+  inputs."nimpng".repo  = "flake-nimble";
+  inputs."nimpng".type  = "github";
+  inputs."nimpng".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimpng".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-jupyternim-0_5_5"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-jupyternim-0_5_5";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
